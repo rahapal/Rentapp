b01ec916cc9caf36fba6732568606b4b8865f7a3
Sushant Rahapal <sushantrahapal@gmail.com>
2022-11-29 15:10:46 +0545

    Added payment Schema

---
+++ lib/Hive_db/payment.dart
@@ -0,0 +1,22 @@
+import 'package:hive/hive.dart';
+
+part 'payment.g.dart';
+
+@HiveType(typeId: 0)
+class Payment {
+  @HiveField(0)
+  int spaceid;
+  @HiveField(1)
+  String date;
+  @HiveField(2)
+  double advanceDeposit;
+  @HiveField(3)
+  double payedAmount;
+
+  Payment({
+    required this.spaceid,
+    required this.date,
+    required this.advanceDeposit,
+    required this.payedAmount,
+  });
+}
---
+++ lib/Hive_db/payment.g.dart
@@ -0,0 +1,50 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'payment.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class PaymentAdapter extends TypeAdapter<Payment> {
+  @override
+  final int typeId = 0;
+
+  @override
+  Payment read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Payment(
+      spaceid: fields[0] as int,
+      date: fields[1] as String,
+      advanceDeposit: fields[2] as double,
+      payedAmount: fields[3] as double,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Payment obj) {
+    writer
+      ..writeByte(4)
+      ..writeByte(0)
+      ..write(obj.spaceid)
+      ..writeByte(1)
+      ..write(obj.date)
+      ..writeByte(2)
+      ..write(obj.advanceDeposit)
+      ..writeByte(3)
+      ..write(obj.payedAmount);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is PaymentAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
